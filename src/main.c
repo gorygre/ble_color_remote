f5a09834940a0c382aa5877cdfdad319836ddf11
Gregory McCall <gregory@grmccall.com>
2023-03-14 18:44:27 +0100

    working garbage prototype

--- src/main.c
+++ src/main.c
@@ -15,13 +15,20 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/sys/byteorder.h>
 
-#define SLEEP_TIME_MS	1
 /*
  * Get button configuration from the devicetree sw0 alias. This is mandatory.
  */
 #define SW0_NODE	DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+#define SW1_NODE	DT_ALIAS(sw1)
+#if !DT_NODE_HAS_STATUS(SW1_NODE, okay)
+#error "Unsupported board: sw1 devicetree alias is not defined"
+#endif
+#define SW2_NODE	DT_ALIAS(sw2)
+#if !DT_NODE_HAS_STATUS(SW2_NODE, okay)
+#error "Unsupported board: sw2 devicetree alias is not defined"
 #endif
 
 static void connect(void);
@@ -36,9 +43,18 @@ static struct bt_gatt_write_params write_params;
 static char data[] = { 0x7E, 0x07, 0x05, 0x03, 0x00, 0xFF, 0x00, 0x00, 0xEF };
 static uint16_t att_handle = 1;
 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
 							      {0});
-static struct gpio_callback button_cb_data;
+static struct k_sem sem0;
+static struct k_sem sem1;
+static struct k_sem sem2;
+static const struct gpio_dt_spec button1 = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios,
+							      {0});
+static const struct gpio_dt_spec button2 = GPIO_DT_SPEC_GET_OR(SW2_NODE, gpios,
+							      {0});
+static struct gpio_callback button0_cb_data;
+static struct gpio_callback button1_cb_data;
+static struct gpio_callback button2_cb_data;
 
 /*
  * The led0 devicetree alias is optional. If present, we'll use it
@@ -51,14 +67,28 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb,
 		    uint32_t pins)
 {
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-	write(default_conn, att_handle);
+	if (pins & 0x2000)
+	{
+		k_sem_give(&sem0);
+	}
+	else if (pins & 0x4000)
+	{
+		k_sem_give(&sem1);
+	}
+	else if (pins & 0x8000)
+	{
+		k_sem_give(&sem2);
+	}
+	else
+	{
+		printk("No effect implemented for pins 0x%X\n", pins);
+	}
 }
 
 void written(struct bt_conn *conn, uint8_t err, struct bt_gatt_write_params* params)
 {
 	printk("Write successful\n");
 	printk("[err] 0x%X\n", err);
-	bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 }
 
 void write(struct bt_conn *conn, uint16_t handle)
@@ -75,6 +105,7 @@ void write(struct bt_conn *conn, uint16_t handle)
 	{
 		printk("Failed to write (err %d)\n", err);
 	}
+	bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 }
 
 static uint8_t discover_func(struct bt_conn *conn,
@@ -92,6 +123,7 @@ static uint8_t discover_func(struct bt_conn *conn,
 	// the characteristic is 2 away from the service
 	att_handle = attr->handle + 2;
 	printk("[ATTRIBUTE] handle %u stored\n", att_handle);
+	write(default_conn, att_handle);
 
 	return BT_GATT_ITER_STOP;
 }
@@ -157,8 +189,7 @@ static void connect(void)
 		return;
 	}
 
-	// TODO: need longer timeout I think
-	err = bt_conn_le_create(&addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT, &default_conn);
+	err = bt_conn_le_create(&addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM(BT_GAP_INIT_CONN_INT_MIN, BT_GAP_INIT_CONN_INT_MAX, 0, 2000), &default_conn);
 	if (err) {
 		printk("Create conn failed (err %d)\n", err);
 	}
@@ -173,30 +204,82 @@ void main(void)
 {
 	int err;
 
-	if (!device_is_ready(button.port)) {
+	k_sem_init(&sem0, 0, 1);
+	k_sem_init(&sem1, 0, 1);
+	k_sem_init(&sem2, 0, 1);
+
+	if (!device_is_ready(button0.port)) {
 		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+		       button0.port->name);
+		return;
+	}
+	
+	if (!device_is_ready(button1.port)) {
+		printk("Error: button device %s is not ready\n",
+		       button1.port->name);
+		return;
+	}
+	
+	if (!device_is_ready(button2.port)) {
+		printk("Error: button device %s is not ready\n",
+		       button2.port->name);
 		return;
 	}
 
-	err = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	err = gpio_pin_configure_dt(&button0, GPIO_INPUT);
 	if (err != 0) {
 		printk("Error %d: failed to configure %s pin %d\n",
-		       err, button.port->name, button.pin);
+		       err, button0.port->name, button0.pin);
 		return;
 	}
 
-	err = gpio_pin_interrupt_configure_dt(&button,
+	err = gpio_pin_configure_dt(&button1, GPIO_INPUT);
+	if (err != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       err, button1.port->name, button1.pin);
+		return;
+	}
+
+	err = gpio_pin_configure_dt(&button2, GPIO_INPUT);
+	if (err != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       err, button2.port->name, button2.pin);
+		return;
+	}
+
+	err = gpio_pin_interrupt_configure_dt(&button0,
 					      GPIO_INT_EDGE_TO_ACTIVE);
 	if (err != 0) {
 		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			err, button.port->name, button.pin);
+			err, button0.port->name, button0.pin);
 		return;
 	}
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
-	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+	err = gpio_pin_interrupt_configure_dt(&button1,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (err != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			err, button1.port->name, button1.pin);
+		return;
+	}
+
+	err = gpio_pin_interrupt_configure_dt(&button2,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (err != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			err, button2.port->name, button2.pin);
+		return;
+	}
+
+	gpio_init_callback(&button0_cb_data, button_pressed, BIT(button0.pin));
+	gpio_init_callback(&button1_cb_data, button_pressed, BIT(button1.pin));
+	gpio_init_callback(&button2_cb_data, button_pressed, BIT(button2.pin));
+	gpio_add_callback(button0.port, &button0_cb_data);
+	gpio_add_callback(button1.port, &button1_cb_data);
+	gpio_add_callback(button2.port, &button2_cb_data);
+	printk("Set up button at %s pin %d\n", button0.port->name, button0.pin);
+	printk("Set up button at %s pin %d\n", button1.port->name, button1.pin);
+	printk("Set up button at %s pin %d\n", button2.port->name, button2.pin);
 
 	if (led.port && !device_is_ready(led.port)) {
 		printk("Error %d: LED device %s is not ready; ignoring it\n",
@@ -223,18 +306,27 @@ void main(void)
 
 	printk("Bluetooth initialized\n");
 
-	connect();
-
-	printk("Press the button\n");
+	printk("Press a button\n");
 	if (led.port) {
 		while (1) {
-			/* If we have an LED, match its state to the button's. */
-			int val = gpio_pin_get_dt(&button);
-
-			if (val >= 0) {
-				gpio_pin_set_dt(&led, val);
+			if (k_sem_take(&sem0, K_MSEC(50)) == 0) {
+				data[4] = 0xFF;
+				data[5] = 0x00;
+				data[6] = 0x00;
+				connect();
 			}
-			k_msleep(SLEEP_TIME_MS);
+			if (k_sem_take(&sem1, K_MSEC(50)) == 0) {
+				data[4] = 0x00;
+				data[5] = 0xFF;
+				data[6] = 0x00;
+				connect();
+			}
+			if (k_sem_take(&sem2, K_MSEC(50)) == 0) {
+				data[4] = 0x00;
+				data[5] = 0x00;
+				data[6] = 0xFF;
+				connect();
+			}
 		}
 	}
 }
